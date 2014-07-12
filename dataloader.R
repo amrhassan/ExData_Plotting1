09b9f39547f6c40ddf6471af84ea0bd14cd3f268
Amr Hassan <amr.hassan@gmail.com>
2014-07-12 23:14:32 +0200

    dataloader.R downloads and reads directly from the ZIP data file now.

--- dataloader.R
+++ dataloader.R
@@ -4,13 +4,19 @@
 
 # Loads and returns the data for the days 2007-02-01 and 2007-02-02.
 data.load <- function() {
-  file.path <- "household_power_consumption.txt"
+  file.url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'  
+  file.path <- "data.zip"
   
-  data <- read.table(file.path, sep=";", header=T, as.is=T, 
+  # Download the data file if it doesn't already exist
+  if (!file.exists(file.path)) {
+    download.file(file.url, file.path, method='curl')
+  }
+  
+  # Read the data into memory
+  data <- read.table(unz(file.path, 'household_power_consumption.txt'), sep=";", header=T, as.is=T, 
                      colClasses=c('character', 'character', rep('numeric', 7)),
                      na.strings=c('?'))
   
+  # Subset to only the relevant days
   data <- data[data$Date=='1/2/2007' | data$Date=='2/2/2007',]
-  
-  # data$DateTime <- as.POSIXct(strptime(paste(data$Date, data$Time), '%d/%m/%Y %H:%M:%S'))
 }
